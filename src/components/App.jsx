1ae4e4ea1b80c7dae0021338b311a8b294c7140a
silvertejp89 <kriss.stockhaus@gmail.com>
2022-06-03 11:14:12 +0200

    changed if else to ternanry operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Login from './Login';
 
-var isLoggedIn = true;
+var isLoggedIn = false;
 
-function conditionallyRender() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function conditionallyRender() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{conditionallyRender()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
+
+//conditional rendering, separations of concerns
 
