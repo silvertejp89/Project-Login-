05690d1cbc8cfc5a6a9b1bceeeffad6334736c92
silvertejp89 <kriss.stockhaus@gmail.com>
2022-06-03 10:58:34 +0200

    add conditional rendering dependinlogged in, split up components for login and input, add props.

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './Login';
+
+var isLoggedIn = true;
+
+function conditionallyRender() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return <Login />;
+  }
+}
+
+function App() {
+  return <div className="container">{conditionallyRender()}</div>;
+}
+
+export default App;
+
